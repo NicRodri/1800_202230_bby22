d685a2d941ebb72047a5a405efddc116bf80916c
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-10-27 22:44:43 -0700

    Finish setting up firebase

---
+++ scripts/firebaseAPI_TEAM22.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARcFv7CFePts0UnHYkK5O9Gl9LLS-hL5U",
+  authDomain: "balance-teambby22.firebaseapp.com",
+  projectId: "balance-teambby22",
+  storageBucket: "balance-teambby22.appspot.com",
+  messagingSenderId: "3885337258",
+  appId: "1:3885337258:web:0f7545ca7a119fc175ded3"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
