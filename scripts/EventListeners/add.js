0f0cb3ad497f68235f349fb6fc1f2c304db7a530
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-10-31 11:13:59 -0700

    add functionality to buttons in modal for + icon

---
+++ scripts/EventListeners/add.js
@@ -0,0 +1,21 @@
+function add(){
+  const task = document.getElementById("New-Task");
+  const activity = document.getElementById("New-Activity");
+  const sleep = document.getElementById("Sleep-Schedule");
+
+  function toTask(){
+    window.location.href = "/html/Tasks_And_Activities/task.html"
+  }
+  function toActivity(){
+    window.location.href = "/html/Tasks_And_Activities/activity.html"
+  }
+  function toSleep(){
+    window.location.href = "/html/Sleep_Time/sleep.html"
+  }
+
+  task.addEventListener("click", toTask)
+  activity.addEventListener("click", toActivity)
+  sleep.addEventListener("click", toSleep)
+}
+
+add();
