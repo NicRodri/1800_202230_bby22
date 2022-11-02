e9abf67be0a7f222ac16427565ae02a5dfa99ead
Haochen Ying <lucasying23@gmail.com>
2022-11-02 15:31:24 -0700

    Notifications6

--- scripts/EventListeners/notifications.js
+++ scripts/EventListeners/notifications.js
@@ -1,3 +1,27 @@
-function notificationSelect(){
-    
+function notificationSelect() {
+    const settings = document.getElementById("settings");
+
+    function getSettings() {
+        window.location.href = "/html/notifications/Settings1.html";
+    }
+
+    settings.addEventListener("click", getSettings);
 }
+
+notificationSelect();
+
+function borderSelect() {
+
+
+
+    const border11 = document.getElementById("border11");
+
+    function getBorder11() {
+        window.location.href = "/html/notifications/Settings2.html";
+    }
+
+    border11.addEventListener("click", getBorder11);
+
+}
+
+borderSelect();
