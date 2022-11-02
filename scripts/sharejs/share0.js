d6b6322d2cedcf2de863849b66df2030523a080f
Bingdididi <zhoubingdica@gmail.com>
2022-11-02 16:29:11 -0700

    create new javascript

---
+++ scripts/sharejs/share0.js
@@ -0,0 +1,11 @@
+function shareSelect() {
+    const shares = document.getElementById("sharebutton");
+
+    function shareSelect() {
+        window.location.href = "/html/share/index1.html";
+    }
+
+    shares.addEventListener("click", shareSelect);
+}
+
+ shareSelect();
