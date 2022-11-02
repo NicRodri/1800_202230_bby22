8c3274b1e5d785307fea917e91c32c7f6858d5f9
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-02 16:03:07 -0700

    Settings navigation works

--- scripts/EventListeners/Settings0.js
+++ scripts/EventListeners/Settings0.js
@@ -10,18 +10,3 @@ function notificationSelect() {
 
 notificationSelect();
 
-function borderSelect() {
-
-
-
-    const border11 = document.getElementById("border11");
-
-    function getBorder11() {
-        window.location.href = "/html/notifications/Settings2.html";
-    }
-
-    border11.addEventListener("click", getBorder11);
-
-}
-
-borderSelect();
---
+++ scripts/EventListeners/Settings1.js
@@ -0,0 +1,13 @@
+function borderSelect() {
+
+  const border11 = document.getElementById("border11");
+
+  function getBorder11() {
+      window.location.href = "/html/notifications/Settings2.html";
+  }
+
+  border11.addEventListener("click", getBorder11);
+
+}
+
+borderSelect();
