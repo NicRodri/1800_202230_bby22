b2ca731327b0ef16797cde441e69a765b22d2757
Bingdididi <zhoubingdica@gmail.com>
2022-11-02 20:00:10 -0700

    add js for close and multi

---
+++ scripts/sharejs/share1.js
@@ -0,0 +1,23 @@
+function multipleSelect() {
+    const multibutton = document.getElementById("multiple");
+
+    function multipleSelect() {
+        window.location.href = "/html/share/index2.html";
+    }
+
+    multibutton.addEventListener("click", multipleSelect);
+}
+
+ multipleSelect();
+ 
+ function closeSelect() {
+    const closebutton = document.getElementById("close");
+
+    function closeSelect() {
+        window.location.href = "/html/share/index0.html";
+    }
+
+    closebutton.addEventListener("click", closeSelect);
+}
+
+ closeSelect();
---
+++ scripts/sharejs/share2.js
@@ -0,0 +1,10 @@
+function closeSelect() {
+    const closebutton = document.getElementById("close");
+
+    function closeSelect() {
+        window.location.href = "/html/share/index0.html";
+    }
+
+    closebutton.addEventListener("click", closeSelect);
+}
+closeSelect();
