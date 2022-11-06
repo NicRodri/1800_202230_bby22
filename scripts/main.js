dd5e10b4d2bd46021b09ee668fea7f732ed74d6d
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-05 22:28:52 -0700

    add sample tasks and activities

--- scripts/main.js
+++ scripts/main.js
@@ -32,71 +32,119 @@ function readQuote() {
 }
 readQuote();        //calling the function
 
-function writeHikes() {
-  //define a variable for the collection you want to create in Firestore to populate data
-  var hikesRef = db.collection("hikes");
-
-  hikesRef.add({
-      code: "BBY01",
-      name: "Burnaby Lake Park Trail",    //replace with your own city?
-      city: "Burnaby",
-      province: "BC",
-      level: "easy",
-      length: "10",
-      details: "Nick goes here regularly",
-      last_updated: firebase.firestore.FieldValue.serverTimestamp()
-  });
-  hikesRef.add({
-      code: "AM01",
-      name: "Buntzen Lake Trail Trail",    //replace with your own city?
-      city: "Anmore",
-      province: "BC",
-      level: "moderate",
-      length: "10.5",
-      details: "Nick goes here regularly",
-      last_updated: firebase.firestore.FieldValue.serverTimestamp()
-  });
-  hikesRef.add({
-      code: "NV01",
-      name: "Mount Seymoure Trail",    //replace with your own city?
-      city: "North Vancouver",
-      province: "BC",
-      level: "hard",
-      length: "8.2",
-      details: "Nick goes here regularly",
-      last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
-  });
-}        
-
-//writeHikes(); // only use once to setup.
-
-function displayCards(collection) {
-  let cardTemplate = document.getElementById("hikeCardTemplate");
-
-  db.collection(collection).get()
-      .then(snap => {
-          //var i = 1;  //if you want to use commented out section
-          snap.forEach(doc => { //iterate thru each doc
-              var title = doc.data().name;        // get value of the "name" key
-              var details = doc.data().details;   // get value of the "details" key
-              var hikeID = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-              let newcard = cardTemplate.content.cloneNode(true);
-
-              //update title and text and image
-              newcard.querySelector('.card-title').innerHTML = title;
-              newcard.querySelector('.card-text').innerHTML = details;
-              newcard.querySelector('.card-image').src = `/images/Test/${hikeID}.jpg`; //Example: NV01.jpg
-
-              //give unique ids to all elements for future use
-              // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-              // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-              // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-              //attach to gallery
-              document.getElementById(collection + "-go-here").appendChild(newcard);
-              //i++;   //if you want to use commented out section
-          })
-      })
+function getTime() {
+  const myDate = new Date();
+  var hour = myDate.getHours();
+  var minute = myDate.getMinutes();
+  let time = hour + ":" + minute;
+  // Will remove the milliseconds at a later time
+  return time;
 }
 
-displayCards("hikes");
+
+function getDate() {
+  const obj = new Date();
+  date = obj.toDateString();
+  // Still need to add comma after day
+  date = date.replace(/(?<=\d) /, ", ");
+  return date;
+}
+
+
+function addTask() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var tasksRef = db.collection("tasks");
+
+  tasksRef.add({
+    code: "Task1",
+    taskName: "Comp1800 lecture",    //replace with your own city?
+    taskDetails: "projects lectures with carly",
+    date: getDate(),
+    timeStart: getTime(),
+    timeEnd: getTime(),
+    repeats: "Weekly",
+    commutingTime: "0",
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()
+  });
+}
+
+//addTask(); // only use once to setup.
+
+function addActivity() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var activitiesRef = db.collection("activities");
+
+  activitiesRef.add({
+    code: "Activity1",
+    activityName: "1510 assignment2",    //replace with your own city?
+    activityDetails: "Assignment 2 consisting of 4 questions",
+    estimatedTime: 10,
+    dueDate: getDate(),
+    commutingTime: 0,
+    urgencyFactor: "Weekly",
+    last_updated: firebase.firestore.FieldValue.serverTimestamp()
+  });
+}
+
+//addActivity(); // only use once to setup.
+
+function displayCardTasks(collection) {
+  let cardTemplate = document.getElementById("taskCardTemplate");
+
+  db.collection(collection).get()
+    .then(snap => {
+      var i = 1;  //if you want to use commented out section
+      snap.forEach(doc => { //iterate thru each doc
+        var title = doc.data().taskName;        // get value of the "name" key
+        var timeStart = doc.data().timeStart;
+        var timeEnd = doc.data().timeEnd;
+        let newcard = cardTemplate.content.cloneNode(true);
+
+        //update title and text and image
+        newcard.querySelector('.card-title').innerHTML = title;
+        newcard.querySelector('.timeStart').innerHTML = timeStart + " - ";
+        newcard.querySelector('.timeEnd').innerHTML = timeEnd;
+
+        //give unique ids to all elements for future use
+        newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+        newcard.querySelector('.timeStart').setAttribute("id", "tStart" + i);
+        newcard.querySelector('.timeEnd').setAttribute("id", "tEnd" + i);
+
+        //attach to gallery
+        document.getElementById(collection + "-go-here").appendChild(newcard);
+        i++;   //if you want to use commented out section
+      })
+    })
+
+}
+
+displayCardTasks("tasks");
+
+function displayCardActivities(collection) {
+  let cardTemplate = document.getElementById("activityCardTemplate");
+
+  db.collection(collection).get()
+    .then(snap => {
+      var i = 1;  //if you want to use commented out section
+      snap.forEach(doc => { //iterate thru each doc
+        var title = doc.data().activityName;
+        var timeEstimated = doc.data().estimatedTime;
+        let newcard = cardTemplate.content.cloneNode(true);
+
+        //update title and text and image
+        newcard.querySelector(".card-title").innerHTML = title;
+        newcard.querySelector(".estimatedTime").innerHTML = "Estimated: " + timeEstimated + " Minutes";
+
+        //give unique ids to all elements for future use
+        newcard.querySelector(".card-title").setAttribute("id", "ctitle" + i);
+        newcard.querySelector(".estimatedTime").setAttribute("id", "estimatedTime" + i);
+
+        //attach to gallery
+        document.getElementById(collection + "-go-here").appendChild(newcard);
+        i++;   //if you want to use commented out section
+      })
+    })
+
+}
+
+displayCardActivities("activities");
