c6940642bd67cddb56a06b1fa7549701f9cb82e2
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-05 17:16:13 -0700

    test firestore

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,33 @@
 function insertName() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid);
-            console.log(user.displayName);
-            user_Name = user.displayName;
-  
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
-            $("#name-goes-here").text(user_Name); //using jquery
-  
-        } else {
-            // No user is signed in.
-        }
-    });
-  }
-  insertName(); //run the function
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if a user is signed in:
+    if (user) {
+      // Do something for the currently logged-in user here: 
+      console.log(user.uid);
+      console.log(user.displayName);
+      user_Name = user.displayName;
+
+      //method #1:  insert with html only
+      //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+      //method #2:  insert using jquery
+      $("#name-goes-here").text(user_Name); //using jquery
+
+    } else {
+      // No user is signed in.
+    }
+  });
+}
+insertName(); //run the function
+
+function readQuote() {
+  db.collection("quotes").doc("Tuesday")                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+    .onSnapshot(somedoc => {                                                               //arrow notation
+      console.log("current document data: " + somedoc.data());                          //.data() returns data object
+      document.getElementById("quote-goes-here").innerHTML = somedoc.data().quote;      //using javascript to display the data on the right place
+
+      //Here are other ways to access key:value data fields
+      //$('#quote-goes-here').text(tuesdayDoc.data().quote);                                       //using jquery object dot notation
+      //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);                                    //using json object indexing
+    })
+}
+readQuote();        //calling the function
