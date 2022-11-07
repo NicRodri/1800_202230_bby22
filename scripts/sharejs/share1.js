93457135c3874406ed47613981edd30d7ce0f93b
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-07 14:56:47 -0800

    put modal in share

--- scripts/sharejs/share1.js
+++ scripts/sharejs/share1.js
@@ -21,3 +21,26 @@ function multipleSelect() {
 }
 
  closeSelect();
+
+
+ function add(){
+    const task = document.getElementById("New-Task");
+    const activity = document.getElementById("New-Activity");
+    const sleep = document.getElementById("Sleep-Schedule");
+  
+    function toTask(){
+      window.location.href = "/html/Tasks_And_Activities/task.html";
+    }
+    function toActivity(){
+      window.location.href = "/html/Tasks_And_Activities/activity.html";
+    }
+    function toSleep(){
+      window.location.href = "/html/Sleep_Time/sleep.html";
+    }
+  
+    task.addEventListener("click", toTask);
+    activity.addEventListener("click", toActivity);
+    sleep.addEventListener("click", toSleep);
+  }
+  
+  add();
