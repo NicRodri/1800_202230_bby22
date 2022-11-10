5775d45e9228f3ae5bbbad280276bfaa75bd3330
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-10 13:57:12 -0800

    allow for sleep to be updated only once

--- scripts/Scheduler/sleep.js
+++ scripts/Scheduler/sleep.js
@@ -1,49 +1,59 @@
+var currentUser;
 const sleepInfo = document.getElementById("sleepInfo");
 const sleepTime = document.getElementById("sleepTime");
 const sleepHours = document.getElementById("sleepHours");
 const sleepSubmit = document.getElementById("submit");
 
-// sleepSubmit.addEventListener('click', (e) => {
-//   e.preventDefault();
+sleepSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  saveUserInfo();
+  populateInfo();
+});
 
-function populateInfo(){
-  firebase.auth().onAuthStateChanged((user) => {
+
+function populateInfo() {
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
 
-      var uid = user.uid;
-      console.log(uid);
-
-      db.collection("users").doc(uid).collection("sleep").doc().get()
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid)
+      //get the document for current user.
+      currentUser.get()
         .then(userDoc => {
-        // Sleep_Time: sleepTime.value,
-        // Sleep_Hours: sleepHours.value,
+          //get the data fields of the user
+          var sleepHoursInfo = userDoc.data().sleepHours;
+          var sleepTimeInfo = userDoc.data().sleepTime;
 
-        var sleepTimeInfo = userDoc.data().sleepTime;
-        var sleepHourInfo = userDoc.data().sleepHours;
-
-        if (sleepTimeInfo != null) {
-          sleepTime.value = sleepTimeInfo;
-        }
-        if (sleepHourInfo != null) {
-          sleepHours.value = sleepHourInfo;
-        }
-      })
-        // .then(() => {
-        //   // activityInfo.reset();
-        //   // window.location.href = "/html/main.html";
-        // });
-
-
-      // ...
+          //if the data fields are not empty, then write them in to the form.
+          if (sleepHoursInfo != null) {
+            sleepHours.value = sleepHoursInfo;
+          }
+          if (sleepTimeInfo != null) {
+            sleepTime.value = sleepTimeInfo;
+          }
+        })
     } else {
-      // User is signed out
-      // ...
+      // No user is signed in.
+      console.log("No user is signed in");
     }
   });
 }
 populateInfo();
 
-
-// });
+function saveUserInfo() {
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+      currentUser.update({
+        sleepHours: sleepHours.value,
+        sleepTime: sleepTime.value
+      }).then(() => {
+        console.log("Document successfully updated!");
+      })
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  })
+}
