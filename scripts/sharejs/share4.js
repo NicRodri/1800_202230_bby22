9446c161b127999f719b3c78d3cb6de137a48eea
Bingdididi <zhoubingdica@gmail.com>
2022-11-14 14:57:07 -0800

    delete the close button

--- scripts/sharejs/share4.js
+++ scripts/sharejs/share4.js
@@ -54,14 +54,16 @@ function showUploadedPicture(){
       image.src = blob;    //assign the "src" property of the "img" tag
   })
 }
+showUploadedPicture();
+
 function uploadContactProfilePic() {
   // Let's assume my storage is only enabled for authenticated users 
   // This is set in your firebase console storage "rules" tab
 
   firebase.auth().onAuthStateChanged(function (user) {
+    
       var fileInput = document.getElementById("mypic-input");   // pointer #1
       const image = document.getElementById("mypic-goes-here"); // pointer #2
-      var cid = contacts.cid;
       // listen for file selection
       fileInput.addEventListener('change', function (e) {
           var file = e.target.files[0];
@@ -70,7 +72,7 @@ function uploadContactProfilePic() {
          
 
           //store using this name
-          var storageRef = storage.ref("images/" + contacts.cid + ".jpg"); 
+          var storageRef = storage.ref("images/" + user.uid + ".jpg"); 
           
           //upload the picked file
           storageRef.put(file) 
@@ -82,7 +84,7 @@ function uploadContactProfilePic() {
           storageRef.getDownloadURL()
               .then(function (url) {   // Get URL of the uploaded file
                   console.log(url);    // Save the URL into users collection
-                  db.collection("users").doc(uid).collection("contacts").doc().set({
+                  db.collection("users").doc(user.uid).update({
 
                       "profilePic": url
                   })
