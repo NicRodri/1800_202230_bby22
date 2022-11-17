47030ad16c3da1e42ed6588867924855399c7c74
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2022-11-17 14:54:28 -0800

    create page for all tasks/activities and a page for all completed tasks/activities

--- scripts/EventListeners/navigationFooter.js
+++ scripts/EventListeners/navigationFooter.js
@@ -1,17 +1,27 @@
 function navigationFooter(){
   const home = document.getElementById("home");
+  const history = document.getElementById("history");
+  const completed = document.getElementById("completed");
+  const person = document.getElementById("person");
+
   function goHome(){
     window.location.href = "/html/main.html";
   }
-
   home.addEventListener("click", goHome);
 
-  const person = document.getElementById("person");
   function goPerson(){
     window.location.href = "/html/Notifications/Settings0.html";
   }
-
   person.addEventListener("click", goPerson);
+
+
+  history.addEventListener("click" , function (e){
+    window.location.href = "/html/All_Tasks_and_Activities/history.html";
+  });
+  completed.addEventListener("click", function (e){
+    window.location.href = "/html/Completed_Tasks_and_Activities/completed.html";
+  });
+
 }
 
 navigationFooter();
